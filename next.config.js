6f59baeba3841ba5254aec60e9ec550fe06ecd58
mortomulu <mfajrihidayat09@gmail.com>
2025-04-10 13:40:24 +0700

    FIX: anomali

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')([
+  'antd',
+  '@ant-design/icons',
+  'rc-util',
+  'rc-picker',
+  'rc-pagination',
+  'dayjs',
+]);
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+module.exports = nextConfig;
