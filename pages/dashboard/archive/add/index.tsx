68e6a0e5983e739a8216925411dc0528e29dbff8
mortomulu <mfajrihidayat09@gmail.com>
2025-04-23 21:13:13 +0700

    feat: add tax archieve manually but still default

---
+++ pages/dashboard/archive/add/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layouts/Layout";
+import TransferTable from "@/components/core/archieve/TransferTable";
+
+const AddArchieveManuallyPage = () => {
+  return (
+    <Layout>
+      <TransferTable />
+    </Layout>
+  );
+};
+
+export default AddArchieveManuallyPage;
